fc121d671c06ae3db39a6f6e066238ab49ba0726
Ahnaf Ahmed Anzum <ahmedanzum23@gmail.com>
2025-10-29 00:39:39 +0600

    multieliment

--- tests/SimpleLocator.spec.js
+++ tests/SimpleLocator.spec.js
@@ -7,8 +7,10 @@ test("Single element locating" , async ({page})  =>{
 await page.goto("https://demo-opencart.com/index.php?route=account/register")
 
 
+
 //general locator
 
+/*
 const firstNameTxt  = page.locator("#input-firstname") ;
 const lastNameTxt = page.locator("#input-lastname");
 const emailTxt = page.locator("#input-email");
@@ -17,6 +19,22 @@ await firstNameTxt.fill("anzum");
 await  lastNameTxt.fill("khan");
 await emailTxt.fill("test@gail.com");
 
+*/
+
+
+ //MULTIELEMENT 
+
+
+
+ const fromControl  = page.locator(".form-control");
+ console.log(await fromControl.count());
+
+ await fromControl.first().fill("Iphone");
+ await fromControl.last().fill("1234");
+ 
+
+await fromControl.nth(1).fill("Henry");
+await fromControl.nth(2).fill("cavil");
 
 
 await page.pause();
