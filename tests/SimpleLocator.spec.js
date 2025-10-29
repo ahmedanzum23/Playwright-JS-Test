ba523dd44abb7f85d6f4e10dd6f33dd43912158d
Ahnaf Ahmed Anzum <ahmedanzum23@gmail.com>
2025-10-29 22:15:02 +0600

    playwright and table

--- tests/SimpleLocator.spec.js
+++ tests/SimpleLocator.spec.js
@@ -4,7 +4,7 @@ test("Single element locating" , async ({page})  =>{
 
 
 
-await page.goto("https://demo-opencart.com/index.php?route=account/register")
+await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/register")
 
 
 
@@ -41,32 +41,40 @@ await fromControl.nth(2).fill("cavil");
 
 //playwright locator
 
-let firstNameTxt = page.getByRole () ;
-let lasttNameTxt = page.getByRole () ;
-let personal = page.getByText();
-const emailTxt =  page.getByLabel();
-const phoneNum =  page.getByPlaceholder();
-const img1 = page.getByAltText();
-const img2 = page.getByAltText();
-const passTxtt = page.getByPlaceholder();
+let firstNameTxt = page.getByRole ("textbox" , {"name": "* First Name"}) ;
+let lasttNameTxt = page.getByRole ("textbox" , {"name": "* Last Name"}) ;
+let personal = page.getByText("Register Account");
+const emailTxt =  page.getByLabel("E-Mail");
+const phoneNum=  page.getByPlaceholder("Telephone");
+const img1 = page.getByAltText("naveenopencart");
+const img2 = page.getByTitle("naveenopencart");
+const passTxtt = page.getByPlaceholder("Password");
 
 
 
 
-await firstNameTxt.fill();
-await firstNameTxt.fill();
-await expect(). toBeVisible();
-await emailTxt.fill();
-await phoneNum.fill("");
+await firstNameTxt.fill("anz123");
+await lasttNameTxt.fill("anzumn123");
+await expect(personal). toBeVisible(); //assertion we write expect
+await emailTxt.fill("ab@gmail.com");
+await phoneNum.fill("01871234456");
+await passTxtt.first().fill("1234");
+await passTxtt.last().fill("1234");
 await expect (img1).toBeVisible();
 await expect (img2).toBeVisible();
-await page.locator ("").click();
-await page.locator ("").click();
+await page.locator ("input[type='checkbox']").click();
+await page.locator ("input[type='submit']").click();
 
 
+// Approach 1: Using textContent()
+let confirmMsg = await page.locator("h1").textContent();
+expect(confirmMsg).toBe("Your Account Has Been Created!");
+expect(confirmMsg).toContain("Created!");
 
-
-
+// Approach 2: Using locator assertions
+let confirmMsgLocator = await page.locator("h1");
+await expect(confirmMsgLocator).toHaveText("Your Account Has Been Created!");
+await expect(confirmMsgLocator).toContainText("Created!");
 
 
 await page.pause();
